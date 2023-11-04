11e16e53d3fa19c453416cf5170aabe00f2e69cc
TruongNBN <mylove.lanhuong@gmail.com>
2023-11-04 23:42:33 +0700

    update JobModel

--- src/models/JobModel.js
+++ src/models/JobModel.js
@@ -2,18 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const JobSchema = new Schema({
-    title: String,
-    description: String,
-    educationLevel: Number,
-    generalTitle: String,
-    level: String,
-    salary: String,
-    location: String,
-    type: String,
-    hours: Number,
-    startTime: Number,
-    applicationDeadline: Number,
-    employerId: String,
+    id: String, // Mã công việc
+    title: String, // Tiêu đề công việc
+    levelId: String, // Mã mức độ quan trọng tìm nhân sự: gấp, cức gấp, bình thường, ...
+    employerId: String, // Mã công ty/nhà tuyển dụng
+    quantity: Number, // Số lượng cần tuyển
+    methodId: Number, // Mã phương thức làm việc
+    probationTime: Number, // Thời gian thử việc
+    salaryUnit: String, // Đơn vị tiền tệ cho lương
+    salaryMin: Number, // Mức lương tối thiểu
+    salaryMax: Number, // Mức lương tối đa
+    applyStartTime: Number, // Thời gian bắt đầu nộp hồ sơ
+    applyEndTime: Number, // Thời gian kết thúc nộp hồ sơ
+    applyCount: Number, // Tổng số hồ sơ đã nộp
+    ageMin: Number, // Độ tuổi tối thiểu
+    ageMax: Number, // Độ tuổi tối đa
+    degreeId: String, // Mã trình độ học vấn
+    genderId: String, // Mã giới tính
+    description: String, // Mô tả công việc
+    totalView: Number, // Tổng số lượt đã xem
 });
 
 const JobModal = mongoose.model('jobs', JobSchema);
