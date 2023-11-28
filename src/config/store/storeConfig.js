1d6332411bc1d8636ded1bf5924b2964dbc5163e
TruongNBN <truongnbn.main@gmail.com>
2023-11-29 05:34:42 +0700

    update job api

---
+++ src/config/store/storeConfig.js
@@ -0,0 +1,9 @@
+const StoreConfig = {
+    job: 'Job',
+    hasJob: 'HasJob',
+
+    employer: 'Employer',
+    hasEmployer: 'HasEmployer',
+};
+
+module.exports = StoreConfig;
--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -1,22 +1,85 @@
+const mongoose = require('mongoose');
+const StoreConfig = require('../config/store/storeConfig');
 const JobModal = require('../models/JobModel');
+const EmployerModel = require('../models/EmployerModel');
 
 class JobController {
     get(req, res, next) {
-        JobModal.find({})
-            .then(jobs => {
-                const data = {};
-                jobs.forEach(job => {
-                    Object.assign(data, {
-                        [job._id] : {
-                            ...job.toObject(),
-                            id: job._id,
-                            _id: undefined
-                        }
+        const data = {
+            [StoreConfig.job]: {},
+            [StoreConfig.hasJob]: {},
+            [StoreConfig.employer]: {},
+        };
+        JobModal.find({}).then(jobs => {
+            const employerIds = [];
+            jobs.forEach(job => {
+                const jobObject = job.toObject();
+                const {
+                    _id,
+                    title,
+                    levelId,
+                    employerId,
+                    quantity,
+                    methodId,
+                    probationTime,
+                    salaryUnit,
+                    salaryMin,
+                    salaryMax,
+                    applyStartTime,
+                    applyEndTime,
+                    applyCount,
+                    ageMin,
+                    ageMax,
+                    degreeId,
+                    genderId,
+                    description,
+                    totalView
+                } = jobObject;
+                Object.assign(data[StoreConfig.job], {
+                    [jobObject._id] : {
+                        id: _id,
+                        title,
+                        levelId,
+                        employerId,
+                        quantity,
+                        methodId,
+                        probationTime,
+                        salaryUnit,
+                        salaryMin,
+                        salaryMax,
+                        applyStartTime,
+                        applyEndTime,
+                        applyCount,
+                        ageMin,
+                        ageMax,
+                        degreeId,
+                        genderId,
+                        description,
+                        totalView,
+                    }
+                });
+                if (data[StoreConfig.hasJob][employerId]) {
+                    data[StoreConfig.hasJob][employerId].push(_id);
+                } else {
+                    Object.assign(data[StoreConfig.hasJob], { [employerId]: [_id] });
+                    employerIds.push(employerId);
+                }
+            });
+            return employerIds;
+        }).then((employerIds) => {
+            employerIds.forEach((employerId) => {
+                const objectId = new mongoose.Types.ObjectId(employerId);
+                EmployerModel.findOne({ _id: objectId }).then((employer) => {
+                    const employerObject = employer.toObject();
+                    const { _id, name, logo, address } = employerObject;
+                    Object.assign(data[StoreConfig.employer], {
+                        [employerObject._id] : { id: _id, name, logo, address }
                     });
                 });
-                res.json(data);
-            })
-            .catch(next);
+            });
+            res.json(data);
+        }).catch(next);
+        
     }
 }
 
